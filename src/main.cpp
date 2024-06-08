43c178eece20d30b00af55b72fa89a9a02f7446e
okezieuc <53785400+okezieuc@users.noreply.github.com>
2024-06-08 10:06:34 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::vector<std::string> builtin_commands = {"exit"};
+
   while (true)
   {
     std::cout << "$ ";
@@ -13,6 +15,51 @@ int main()
     std::string input;
     std::getline(std::cin, input);
 
+    std::vector<std::string> command_vector = parse_command_to_string_vector(input);
+
+    if (command_vector.size() == 0)
+    {
+      continue;
+    }
+
+    // handle builtin commands
+    if (std::find(builtin_commands.begin(), builtin_commands.end(), command_vector[0]) != builtin_commands.end())
+    {
+      if (command_vector[0] == "exit")
+      {
+        int exit_code = std::stoi(command_vector[1]);
+        return exit_code;
+      }
+
+      continue;
+    }
+
     std::cout << input << ": command not found\n";
   }
+}
+
+std::vector<std::string> parse_command_to_string_vector(std::string command)
+{
+  std::vector<std::string> args;
+  std::string arg_acc = "";
+
+  for (char c : command)
+  {
+    if (c == ' ')
+    {
+      args.push_back(arg_acc);
+      arg_acc = "";
+    }
+    else
+    {
+      arg_acc += c;
+    }
+  }
+
+  if (arg_acc != "")
+  {
+    args.push_back(arg_acc);
+  }
+
+  return args;
 }
