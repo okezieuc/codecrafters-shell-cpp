0a854bbde8dbfad432a0b5f61fc82c1fb4a34041
okezieuc <53785400+okezieuc@users.noreply.github.com>
2024-06-08 09:32:23 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-  
-  std::string input;
-  std::getline(std::cin, input);
+  while (true)
+  {
+    std::cout << "$ ";
 
-  std::cout << input << ": command not found\n";
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::cout << input << ": command not found\n";
+  }
 }
